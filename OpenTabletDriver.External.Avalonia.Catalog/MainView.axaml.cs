81f7992f49921352d5b578eae3c62ab5897dfe37
Gess1t <tintamare91@gmail.com>
2024-03-17 20:17:35 +0100

    Added Catalog for controls made so far

---
+++ OpenTabletDriver.External.Avalonia.Catalog/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OpenTabletDriver.External.Avalonia.Catalog;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ OpenTabletDriver.External.Avalonia.Catalog/Pages/AreaDisplayPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OpenTabletDriver.External.Avalonia.Catalog.Pages;
+
+public partial class AreaDisplayPage : UserControl
+{
+    public AreaDisplayPage()
+    {
+        InitializeComponent();
+    }
+}
