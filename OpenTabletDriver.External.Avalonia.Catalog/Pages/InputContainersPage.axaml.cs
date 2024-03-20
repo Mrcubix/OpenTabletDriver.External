4db5db7ff7f9bd8fb24fbd95613006802ce04a07
Gess1t <tintamare91@gmail.com>
2024-03-20 18:27:52 +0100

    Added DoubleInput to the catalog

---
+++ OpenTabletDriver.External.Avalonia.Catalog/Pages/InputContainersPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OpenTabletDriver.External.Avalonia.Catalog.Pages;
+
+public partial class InputContainersPage : UserControl
+{
+    public InputContainersPage()
+    {
+        InitializeComponent();
+    }
+}
