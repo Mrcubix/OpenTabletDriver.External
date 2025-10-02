96a4837e781ef42fab9d38926129070eae447fba
Gess1t <tintamare91@gmail.com>
2025-10-02 22:10:14 +0200

    Add bool as an optional argument for BindingEditorDialogViewModel

--- OpenTabletDriver.External.Avalonia/ViewModels/BindingEditorViewModel.cs
+++ OpenTabletDriver.External.Avalonia/ViewModels/BindingEditorViewModel.cs
@@ -6,14 +6,14 @@ namespace OpenTabletDriver.External.Avalonia.ViewModels;
 
 #nullable enable
 
-public partial class BindingEditorDialogViewModel : ViewModelBase
+public partial class BindingEditorDialogViewModel(bool doConvertKeysToEto = false) : ViewModelBase
 {
     [ObservableProperty]
     private SerializablePluginSettingsStore? _store = null!;
 
     public event EventHandler ClearRequested = null!;
 
-    public bool DoConvertKeysToEto { get; init; }
+    public bool DoConvertKeysToEto { get; } = doConvertKeysToEto;
 
     public void Clear()
     {
